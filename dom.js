d0863b82d4446eb3d6cbb3829dd6adb4adbb51cc
asad <asaduzzamanprogrammer@gmail.com>
2024-02-16 15:26:02 +0600

    24-3

--- dom.js
+++ dom.js
@@ -1,10 +1,17 @@
 
-const allHead = document.getElementsByTagName("h1");
-for(const head of allHead){
-    console.log(head);
-}
+// const allHead = document.getElementsByTagName("h1");
+// for(const head of allHead){
+//     console.log(head);
+// }
 
-const innerText = document.getElementsByTagName("li");
-for(const inner of innerText){
-    console.log(inner.innerText)
+// const innerText = document.getElementsByTagName("li");
+// for(const inner of innerText){
+//     console.log(inner.innerText)
+// }
+
+
+// document.getElementById("jhinuk")
+const sawon = document.getElementsByClassName("imp-place")
+for(const asad of sawon){
+    console.log(asad.innerText)
 }
