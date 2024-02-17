9872a0ca7d4bf044350a4e5a073473738d748d0f
asad <asaduzzamanprogrammer@gmail.com>
2024-02-17 11:29:00 +0600

    24-4

--- dom.js
+++ dom.js
@@ -11,13 +11,19 @@
 
 
 // document.getElementById("jhinuk")
-const sawon = document.getElementsByClassName("imp-place")
-for(const asad of sawon){
-    console.log(asad.innerText)
+// const sawon = document.getElementsByClassName("imp-place")
+// for(const asad of sawon){
+//     console.log(asad.innerText)
+// }
+
+// const changes = document.getElementById("jhinuk");
+// changes.innerText = "koi koi koi";
+
+// const chan = document.getElementById("come");
+// chan.innerText = "koi koi koi"
+
+
+const ojil = document.querySelectorAll(".sec li");
+for (const bal of ojil){
+    console.log(bal.innerText)
 }
-
-const changes = document.getElementById("jhinuk");
-changes.innerText = "koi koi koi";
-
-const chan = document.getElementById("come");
-chan.innerText = "koi koi koi"
