2bab2eb96852630d64b13bf01be7a28298f022b9
asad <asaduzzamanprogrammer@gmail.com>
2024-02-17 14:04:47 +0600

    24-7

---
+++ append.js
@@ -0,0 +1,17 @@
+// console.log("append to html")
+// const newLi = document.getElementById("append");
+// const liIteam = document.createElement("li")
+// liIteam.innerText = 'asam bon'
+
+// newLi.appendChild(liIteam)
+
+// const placesList = document.getElementById("places-list")
+// const li = document.createElement('li')
+// li.innerText = 'pahartoli bon'
+// placesList.appendChild(li);
+
+
+const add = document.getElementById("mang")
+const addMul = document.createElement(li);
+addMul.innerText = 'pahartoli bon'
+add.appendChild(addMul)
